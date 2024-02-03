79c525370de952f363c052432a9acc99fca6919c
wade <wadecheng3301@gmail.com>
2024-02-02 19:11:47 -0500

    gs

--- gamestate.py
+++ gamestate.py
@@ -5,16 +5,16 @@ class Notes:
     """data structure for efficient Note operations"""
 
     def __init__(self, notes_filepath: str):
-        self.notes_arr: list[bool] = ...
+        self.notes_arr: list[bool] = None
 
 
 class Map:
     def __init__(self, mapfolder_filepath: str):
         # pass in file args
-        self.author: str = ...
-        self.audio: pygame.mixer.Sound = pygame.mixer.Sound(...)
-        self.notes: Notes = Notes(...)
-        self.bpm: int = ...
+        self.author: str = "temp"
+        self.audio: pygame.mixer.Sound = None
+        self.notes: Notes = None
+        self.bpm: int = None
 
 
 class GameState:
@@ -24,5 +24,8 @@ class GameState:
     def __init__(self, example_param: int=0) -> None:
         self.example_param = example_param
         self.playing = True
+        self.hitcircle = pygame.image.load('assets/hit_circle_basic.png')
+        # rendered_hitcircle_locs: a list of the x-positions of all the hitcircles to be rendered. can be off screen.
+        self.rendered_hitcircle_locs: list[float] = []
 
 
