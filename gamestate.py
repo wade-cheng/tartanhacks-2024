81d265869bb2628451c45367561cb510039ce001
wade <wadecheng3301@gmail.com>
2024-02-02 19:02:17 -0500

    remove old code

--- gamestate.py
+++ gamestate.py
@@ -1,19 +1,28 @@
 import pygame
 from pygame.locals import *
 
-TILE_SIZE = 50
-MAP_TILEWIDTH, MAP_TILEHEIGHT = 10, 10
-MAP_WIDTH, MAP_HEIGHT = TILE_SIZE * MAP_TILEWIDTH, TILE_SIZE * MAP_TILEHEIGHT
-BG_COLOR = (255, 255, 255)
-PLAYER_WIGGLE_EVENT = pygame.USEREVENT + 1
-PLAYER_MOVE_EVENT = pygame.USEREVENT + 2
+class Notes:
+    """data structure for efficient Note operations"""
+
+    def __init__(self, notes_filepath: str):
+        self.notes_arr: list[bool] = ...
+
+
+class Map:
+    def __init__(self, mapfolder_filepath: str):
+        # pass in file args
+        self.author: str = ...
+        self.audio: pygame.mixer.Sound = pygame.mixer.Sound(...)
+        self.notes: Notes = Notes(...)
+        self.bpm: int = ...
+
 
 class GameState:
     """holds the state of the game.
     when adding parameters, include a default value
-    
     so previous code doesn't break"""
     def __init__(self, example_param: int=0) -> None:
         self.example_param = example_param
         self.playing = True
+
 
