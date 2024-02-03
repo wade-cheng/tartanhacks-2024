54f245036a74bed65418950bd2bb3aa980afee6b
Danny Peelen <dapeelen@gmail.com>
2024-02-02 20:03:02 -0500

    Changed the Map in read_file

--- read_file.py
+++ read_file.py
@@ -20,9 +20,9 @@ class Map:
     def __init__(self, maps_filepath: str):
         # pass in file args
         self.author: str = maps_filepath[1]
-        self.audio: Sound = pygame.mixer.Sound(...)
+        self.audio: Sound = pygame.mixer.Sound(maps_filepath[4])
         self.notes: Notes = Notes(maps_filepath)
-        self.bpm: int = maps_filepath[5]
+        self.bpm: int = maps_filepath[7]
 
 
 
