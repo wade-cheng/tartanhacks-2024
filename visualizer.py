de7a2dfcc8d9d7d798519664f78a598ac7068760
wade <wadecheng3301@gmail.com>
2024-02-02 20:46:43 -0500

    upda

--- visualizer.py
+++ visualizer.py
@@ -1,42 +1,10 @@
 from constants import *
 from gamestate import GameState
 import pygame
-"""
-    # class Note:
-#     def __init__(self, count: int):
-#         # duration in counts
-#         self.count = count
-	
-class Notes:
-   data structure for efficient Note operations
-
-    def __init__(self, notes_filepath: str):
-        self.notes_arr: list[bool] = ...
-
-    def other methods idk
-"""
-# class Map:
-#     def __init__(self, mapfolder_filepath: str):
-#         file = open(mapfolder_filepath, "r")
-#         file.readLine()
-#         self.author: str = file.readLine()
-#         file.readLine()
-#         file.readLine()
-#         self.bpm: int = file.readLine()
-#         self.audio: pygame.mixer.Sound = pygame.mixer.Sound(file.readLine())
-#         self.notes: list[bool] = file.readLine()
-        
-
-#         file.close()
-
-# def notes_arr (notes: list[bool]):
-#     leng = len(notes)
-#     for i in range(notes):
-        
+from map import Map
 
 
-
-def notesstream(m : Map, gamestate: GameState):
+def notesstream(m: Map, gamestate: GameState):
     notes : list[bool] = m.notes.notes_arr
     # sync current time to start idx instead of it always zero
     startIdx : int = 0
